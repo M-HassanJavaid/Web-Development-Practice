63301e6ff2d04ec8be82aa2534e4ee63ec7e862c
Hassan Javaid <javaidhassan464@gmail.com>
2025-02-02 20:31:31 +0500

    learn increment and dercrement

---
+++ Module-2/JavaScript/class-4/app.js
@@ -0,0 +1,12 @@
+// Today Topics 
+// 1. Pre increment 
+// 2. Post increment
+// 3. Pre decrement
+// 4. Post decrement
+
+let x = 10;
+console.log(++x); //pre increment
+
+let y = 8;
+console.log(y++); //post increment
+         
